8153cc23496a27cadbc367384c880eba26cd9177
ismael escalante <escalantedevjose@gmail.com>
2023-07-24 16:06:46 -0300

    command echo

--- app/Models/OrderLine.php
+++ app/Models/OrderLine.php
@@ -4,10 +4,35 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class OrderLine extends Model
 {
     use HasFactory;
 
     protected $table = 'orders_lines';
+
+    protected $fillable = [
+        'product_id',
+        'order_id',
+        'qty'
+    ];
+
+    protected $appends = ['subtotal'];
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function order(): BelongsTo
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function getSubtotalAttribute()
+    {
+        $this->load('product');
+        return $this->qty * $this->product['cost'];
+    }
 }
